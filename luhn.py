5e030e37e61330706ff7857c040a280e4153c23d
Ogutu Brian <codingbrian58@gmail.com>
2018-12-26 01:35:29 +0300

    modified data collection function

--- luhn.py
+++ luhn.py
@@ -1,9 +1,12 @@
 import xlrd
-from
+from typing import List
 
-def get_required_data()->list:
-    workbook = xlrd.open_workbook(
-        "Uday - 1K Sample PAN for Upwork - 24-12-2018.xlsx")
+
+def get_required_data(file: str)->List:
+    """
+    Opens an file and retunrns the data in first column of the sheet
+    """
+    workbook = xlrd.open_workbook(file)
     sheets = workbook.sheet_names()
     required_data = []
     for sheet_name in sheets:
@@ -12,3 +15,7 @@ def get_required_data()->list:
             row_values = sh.row_values(rownum)
             required_data.append((row_values[0]))
     return required_data
+
+if __name__ == "__main__":
+    dataset = get_required_data("Uday - 1K Sample PAN for Upwork - 24-12-2018.xlsx")
+    print("The length of the dataset is {}".format(len(dataset)))
