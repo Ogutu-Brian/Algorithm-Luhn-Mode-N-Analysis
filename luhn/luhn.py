ee379eafb8a436fc7988c61fda09b2efa81fa802
Ogutu Brian <codingbrian58@gmail.com>
2018-12-26 17:35:26 +0300

    Added the changing of number to base function during doubling

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -40,14 +40,15 @@ def mapping_to_code_points(pan_list: List)->List:
     return new_pan_list
 
 
-def checksum(pan_list: List)->List:
-    """does the summation of numbers in the list in accordance with luhn algorithm
+def double(pan_list: List)->List:
+    """doubles the digits in accordance with luhn algorithm
     >>>checksum([1, 1, 4, 3, 5, 2, 7, 5, 7, 12])
     >>>[1, 2, 4, 6, 5, 4, 7, 10, 7, 24]
     """
     index = len(pan_list) - 1
     while index > 0:
         pan_list[index] += pan_list[index]
+        pan_list[index] = decimal_to_base_n(pan_list[index], 35)
         index -= 2
     return pan_list
 
@@ -60,18 +61,73 @@ def is_sum_mod_10(pan_list: List)->bool:
     return sum(pan_list) % 10 == 0
 
 
+def decimal_to_base_n(number: int, base: int)->int:
+    """Converts a decimal number into a given base
+    >>>decimal_to_base_n(10,6)
+    >>>14
+    """
+    m = []
+    while number != 0:
+        d = int(number/base)
+        r = number % base
+        m.insert(0, r)
+        number = d
+    index = len(m) - 1
+    multiplier = 1
+    values = []
+    while index >= 0:
+        values.append(m[index]*multiplier)
+        multiplier *= 10
+        index -= 1
+    return sum(values)
+
+
+def Reduce(pan_list: List)->List:
+    """Reduces multiple digit numbers into single digits depending on the base
+    >>>Reduce([1, 2, 4, 6, 5, 4, 7, 10, 7, 24])
+    >>>[1, 2, 4, 6, 5, 4, 7, 1, 7, 6]
+    """
+    reduced_list = []
+    for item in pan_list:
+        if item >= 10:
+            reduced_list.append(sum([int(digit) for digit in str(item)]))
+        else:
+            reduced_list.append(item)
+    return reduced_list
+
+
+def is_mod_n(number: int, base: int)->bool:
+    """Checks if number is of a given mode or not"
+    >>>is_mod_n(70,35)
+    >>>True
+    >>>is_mod_n(36,35)
+    >>>False
+    """
+    return number % base == 0
+
+
 if __name__ == "__main__":
     dataset = get_required_data(
         "Uday - 1K Sample PAN for Upwork - 24-12-2018.xlsx")
     split_data = split_pan(dataset)
     mapped_data = [mapping_to_code_points(item) for item in split_data]
-
+    # reduced_data = [Reduce(item) for item in base_35_data]
+    doubled_data = []
+    reduced_data = []
+    valid_pans = []
+    invalid_pans = []
     for item in mapped_data:
-        print("sum is {}".format(sum(item)))
-    # summed_data = [checksum(item) for item in mapped_data]
-    # for item in summed_data:
-    #     if not is_sum_mod_10(item):
-    #         print(item)
-    #         print("The sum is {}".format(sum(item)))
-    #         print("There was a glitch in the algorithm")
-    #         break
+        doubled_list = double(item[0:9])
+        doubled_list.append(item[9])
+        doubled_data.append(doubled_list)
+    for item in mapped_data:
+        reduced_list = Reduce(item)
+        reduced_list.append(item[9])
+        reduced_data.append(reduced_list)
+    for item in reduced_data:
+        if is_mod_n(sum(item), 35):
+            valid_pans.append(item)
+        else:
+            invalid_pans.append(item)
+    print("The number of valid PAN numbers is {}".format(len(valid_pans)))
+    print("The number of valid PAN numbers is {}".format(len(invalid_pans)))
--- tests/test_data_manipulation.py
+++ tests/test_data_manipulation.py
@@ -1,5 +1,5 @@
 from luhn.luhn import (split_pan, mapping_to_code_points,
-                       checksum, is_sum_mod_10)
+                       double, is_sum_mod_10, decimal_to_base_n, Reduce, is_mod_n)
 import unittest
 
 
@@ -29,7 +29,7 @@ class TestStringManipulation(unittest.TestCase):
     def test_check_sum(self):
         """test if the checksum list is accurate"""
         expected_list = [1, 2, 4, 6, 5, 4, 7, 10, 7, 24]
-        actual_list = checksum([1, 1, 4, 3, 5, 2, 7, 5, 7, 12])
+        actual_list = double([1, 1, 4, 3, 5, 2, 7, 5, 7, 12])
         message = "Expected {} but returned {}".format(
             expected_list, actual_list)
         self.assertEqual(expected_list, actual_list, message)
@@ -42,3 +42,46 @@ class TestStringManipulation(unittest.TestCase):
         message = "Expected {} but returned {}".format(
             expected_value, actual_value)
         self.assertEqual(expected_value, actual_value, message)
+
+    def test_decimal_to_base_n(self):
+        """Tests the conversion of any number into a given base"""
+        expected_value = 14
+        actual_value = decimal_to_base_n(10, 6)
+        message = "Expected {} but returned {}".format(
+            expected_value, actual_value)
+        self.assertEqual(expected_value, actual_value, message)
+
+        expected_value = 10
+        actual_value = decimal_to_base_n(6, 6)
+        message = "Expected {} but returned {}".format(
+            expected_value, actual_value)
+        self.assertEqual(expected_value, actual_value, message)
+
+        expected_value = 2
+        actual_value = decimal_to_base_n(2, 6)
+        message = "Expected {} but returned {}".format(
+            expected_value, actual_value)
+        self.assertEqual(expected_value, actual_value, message)
+
+    def test_reduction_of_list(self):
+        """Tests if a list has been resuces successfully to the required number"""
+        expected_list = [1, 2, 4, 6, 5, 4, 7, 1, 7, 6]
+        actual_list = Reduce([1, 2, 4, 6, 5, 4, 7, 10, 7, 24])
+        message = "Expected {} but returned {}".format(
+            expected_list, actual_list)
+        self.assertEqual(expected_list, actual_list, message)
+
+    def test_is_mod_n(self):
+        """Checks if a given number is of a given mod using modular arithmetic"""
+        expected = True
+        actual = is_mod_n(70, 35)
+        message = "Expected {} but returned {}".format(expected, actual)
+        self.assertEqual(expected, actual, message)
+
+    def test_is_not_mod_n(self):
+        """Checks if a given number is not of mod n using modular arithmetic"""
+        expected = False
+        actual = is_mod_n(71, 35)
+        message = "Expected {} but returned {}".format(
+            expected, actual)
+        self.assertEqual(expected, actual, message)
